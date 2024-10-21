4fbdc22aef7a5e00b3c7fecce1742661a1a97696
luisgerman <27436197+LuisGermanDev@users.noreply.github.com>
2024-10-21 02:42:42 -0400

    rutas de registro arreglado

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 const Register = () => {
-  const [formData, setFormData] = useState({ nombre: '', email: '', password: '' });
-
+  const [formData, setFormData] = useState({
+    nombre: "",
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
@@ -11,20 +15,38 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:3000/register', formData);
-      alert('Usuario registrado');
+      await axios.post("http://localhost:3000/register", formData);
+      alert("Usuario registrado");
+      navigate('/')
     } catch (error) {
-      alert('Error en el registro');
+      alert("Error en el registro");
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="nombre" placeholder="Nombre" onChange={handleChange} />
-      <input type="email" name="email" placeholder="Email" onChange={handleChange} />
-      <input type="password" name="password" placeholder="Contraseña" onChange={handleChange} />
+      <input
+        type="text"
+        name="nombre"
+        placeholder="Nombre"
+        onChange={handleChange}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Contraseña"
+        onChange={handleChange}
+      />
       <button type="submit">Registrar</button>
-      <p>¿Ya tienes cuenta? <Link to="/login">Inicia sesión aquí</Link></p>
+      <p>
+        ¿Ya tienes cuenta? <Link to="/">Inicia sesión aquí</Link>
+      </p>
     </form>
   );
 };
